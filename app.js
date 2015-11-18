72fdc0acb105c59e3a4b36ff251de75f96455bf0
Coleman Foley <colemanfoley@gmail.com>
2015-11-17 20:50:40 -0800

    Set up libs and basic initial layout. Show most popular repos in last 30 days by default.

---
+++ app.js
@@ -0,0 +1,15 @@
+var baseURL = 'https://api.github.com/search';
+var repoSearchURL = baseURL + '/repositories?';
+
+
+var currentDate = new Date();
+var popularRepoURL = repoSearchURL + 'q=created:>' + moment().subtract(30, 'days').toISOString() + '&sort=stars&order=desc';
+
+$(document).ready( function() {
+	$.get( popularRepoURL, function (data) {
+		console.log(data);
+		_.each(data.items, function(item) {
+			$('#repo-list').append('<div>' + item.name + '</div>');
+		})
+	});
+} );
