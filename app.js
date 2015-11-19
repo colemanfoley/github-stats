353522306308d5e7b125086f08a879ed2f883cf0
Coleman Foley <colemanfoley@gmail.com>
2015-11-19 15:41:57 -0800

    Flesh out repo list item template a bit more.

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ var popularRepoURL = repoSearchURL + 'q=created:>' + moment().subtract(30, 'days
 $(document).ready( function() {
 	$.get( popularRepoURL, function (data) {
 		console.log(data);
-		_.each(data.items.slice(0,10), function(item) {
-			var itemContainer = $('<div class="container">');
-			itemContainer.append('<span class="title">' + item.name + '</span>');
+		_.each(data.items.slice(0,10), function(item, index) {
+			var itemContainer = $('<div class="item-container">');
+			itemContainer.append('<span class="title">' + (index + 1) + '.' + item.name + '</span>');
 			itemContainer.append('<span class="stars">Stars: ' + item.stargazers_count + '</span>');
 			$('#repo-list').append(itemContainer);
 		})
